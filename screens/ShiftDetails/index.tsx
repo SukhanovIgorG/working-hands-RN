fe929f832beefb8c416d6c21ab26c377306e1842
SukhanovIgorG <sukhanovgarik@yandex.ru>
2025-09-19 17:03:03 +0300

    feat: create shift details screen

---
+++ screens/ShiftDetails/index.tsx
@@ -0,0 +1,12 @@
+import { Layout, Text } from '@ui-kitten/components';
+import { observer } from 'mobx-react-lite';
+
+const ShiftDetailsScreen = () => {
+  return (
+    <Layout>
+      <Text>ShiftDetails</Text>
+    </Layout>
+  );
+};
+
+export default observer(ShiftDetailsScreen);
